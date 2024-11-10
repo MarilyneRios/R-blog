b261b967cc3bd6cf8fc186b49c1cca765f7141ec
MarilyneRios <rios.marilyne@gmail.com>
2024-11-10 22:21:55 +0100

    install vite react + taillwind + react router

--- client/README.md
+++ client/README.md
@@ -1,7 +1,19 @@
-# React + Vite
+# R-Blog
 
-# Tailwindcss
+## React + Vite
+https://vite.dev/guide/
+
+## Tailwings
+https://tailwindcss.com/docs/guides/vite
+
 ````
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
+````
+
+## React Router
+https://reactrouter.com/en/main
+
+````
+npm install react-router-dom 
 ````
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// imports pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Lessons from './pages/Lessons';
+
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-3xl text-red-500'>App page</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/lessons' element={<Lessons />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
