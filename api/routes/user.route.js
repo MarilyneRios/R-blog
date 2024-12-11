21e0a7d44b0b4bf7f73a434536f8f825f9ae4692
MarilyneRios <rios.marilyne@gmail.com>
2024-12-11 19:18:03 +0100

    test API route avec le dossier controllers

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import {test} from '../Controllers/user.controller.js'; // think to add .js
 
 const router = express.Router();
 
-router.get('/test2', (req, res) => {
-    res.json({ message  : 'API is working on route file!!'})
-})
+router.get('/test2', test);
 
 export default router;
