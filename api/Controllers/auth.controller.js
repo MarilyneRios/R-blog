9a4ff8c0ec7721e484f64c0ea22e824107a3150d
MarilyneRios <rios.marilyne@gmail.com>
2024-12-11 20:02:32 +0100

    auth.controller signup

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -1,3 +1,30 @@
+import User from '../models/user.model.js';
+
 export const signup = async (req, res, next) => {
-    console.log(req.body);
+    const {username, email, password} = req.body;
+
+    // vérifier que les inputs soient remplis    
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    res.status(500).json ({message: error.message});
+  }
 }
